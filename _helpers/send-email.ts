b91252d01b8b9a5332797f53516e1256f2821703
John Paul Taray <your_email@example.com>
2026-05-20 19:32:06 +0800

    Update _helpers/send-email.ts

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,34 +1,6 @@
 import nodemailer from 'nodemailer';
-import configData from '../config.json';
-
-interface SmtpOptions {
-  host: string;
-  port: number;
-  auth: {
-    user: string;
-    pass: string;
-  };
-}
-
-interface Config {
-  emailFrom: string;
-  smtpOptions: SmtpOptions;
-}
-
-const config = configData as Config;
-
-export default async function sendEmail({
-  to,
-  subject,
-  html,
-  from = config.emailFrom,
-}: {
-  to: string;
-  subject: string;
-  html: string;
-  from?: string;
-}) {
-  const transporter = nodemailer.createTransport(config.smtpOptions);
-
-  await transporter.sendMail({ from, to, subject, html });
+import config from '../config.json';
+export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
+    const transporter = nodemailer.createTransport(config.smtpoptions);
+await transporter.sendMail({ from, to, subject, html });
 }
