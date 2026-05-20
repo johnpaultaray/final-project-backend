76810fa2d5dda747f5328774f3d7ec2262fb8bd0
John Paul Taray <your_email@example.com>
2026-05-20 19:46:09 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -13,17 +13,15 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (_origin, callback) => callback(null, true), credentials: true }));
-
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 // api routes
 app.use('/accounts', accountsController);
 
 // swagger docs route
-app.use('/api-docs', ...swaggerDocs);
-
+app.use('/api-docs', swaggerDocs);
 // global error handler
 app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log('Server listening on port ' + port));
