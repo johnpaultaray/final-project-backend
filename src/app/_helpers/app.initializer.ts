6ad832dc2e818e4e8e4a9ca9301b2ea4b35d9cea
John Paul Taray <your_email@example.com>
2026-05-22 01:56:26 +0800

    Fix build configs for deployment

--- src/app/_helpers/app.initializer.ts
+++
@@ -1,9 +0,0 @@
-import { AccountService } from '@app/_services/account.service';
-import { catchError, of } from 'rxjs';
-
-export function appInitializer(accountService: AccountService) {
-    return () => accountService.refreshToken()
-        .pipe(
-            catchError(() => of())
-        );
-}
