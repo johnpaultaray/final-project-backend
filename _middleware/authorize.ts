ba79e2a023e75581dad431dda58d1915867f9567
John Paul Taray <your_email@example.com>
2026-05-25 19:21:13 +0800

    Update to fix module issue in Authorize.ts

--- _middleware/authorize.ts
+++ _middleware/authorize.ts
@@ -1,8 +1,7 @@
 import { expressjwt as jwt } from 'express-jwt';
-import config from '../config.json';
 import db from '../_helpers/db';
 
-const { secret } = config;
+const secret = process.env.JWT_SECRET!;
 
 export default function authorize(roles: any = []) {
     if (typeof roles == 'string') {
